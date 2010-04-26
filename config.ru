fbcd533920286c9307e0e5a5d5981307b8efebdf
Adam Holt <Ads.noob@gmail.com>
2010-04-26 20:23:03 +0100

    Intial app install

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+
+require 'connect4'
+
+run Connect4::App
---
+++ connect4.rb
@@ -0,0 +1,7 @@
+require 'mongomapper'
+
+class Connect4 < Sinatra::Base
+  get '/game/:id' do
+    # Do something
+  end
+end
